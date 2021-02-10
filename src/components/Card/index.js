6840ea6c43daddd9479514681632c785ca1eba68
Valery <valery22051998@gmail.com>
2021-02-10 14:25:42 +0300

    add Header and Card components

---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css'
+function Card() {
+    return (
+        <div className="Card">
+            <h4>Card Name</h4>
+            <hr />
+            <p>Text</p>
+        </div>
+    )
+}
+
+export default Card;
