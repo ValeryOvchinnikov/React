ae72be6647bded3cd4d7553bcbb31e5524adfaae
Valery <valery22051998@gmail.com>
2021-02-11 23:15:28 +0300

    Fix code formatting

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './Card.css'
+import React from "react";
+import "./Card.css";
 function Card() {
-    return (
-        <div className="Card">
-            <h4>Card Name</h4>
-            <hr />
-            <p>Text</p>
-        </div>
-    )
+  return (
+    <div className="Card">
+      <h4>Card Name</h4>
+      <hr />
+      <p>Text</p>
+    </div>
+  );
 }
 
 export default Card;
