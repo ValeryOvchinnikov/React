f772252cb87a9851387cc9ac37865e046c7f04ba
Valery <valery22051998@gmail.com>
2021-02-12 15:03:09 +0300

    add checkbox with color change

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
-const Card = (props) => (
-    <div className="card">
-        <h4 className="cardTitle">{props.title}</h4>
-        <hr className="cardLine" />
-        <p>{props.text}</p>
-    </div>
-);
+const Card = (props) => {
+    const [isChecked, setChecked] = useState(false);
+    return (
+        <div
+            style={{ backgroundColor: isChecked ? '#5E4BD8' : '#2C17B1' }}
+            className="card"
+        >
+            <div>
+                <h4 className="cardTitle">{props.title}</h4>
+                <input
+                    className="cardInput"
+                    type="checkbox"
+                    onClick={() => setChecked(!isChecked)}
+                />
+            </div>
+            <hr className="cardLine" />
+            <p>{props.text}</p>
+        </div>
+    );
+};
 
 export default Card;
