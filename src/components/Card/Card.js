ea381e74db9aaf33e50950a339f0bdfc809d206d
Valery <valery22051998@gmail.com>
2021-02-15 13:33:34 +0300

    refactoring code

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,14 +9,14 @@ const Card = props => {
             style={{ backgroundColor: isChecked ? '#5E4BD8' : '#2C17B1' }}
             className="card"
         >
-            <div>
+            <>
                 <h4 className="cardTitle">{props.title}</h4>
                 <input
                     className="cardInput"
                     type="checkbox"
                     onClick={switchColor}
                 />
-            </div>
+            </>
             <hr className="cardLine" />
             <p>{props.text}</p>
         </div>
