ae56cc9bac1cfd69a896c601183c96aa32d7d832
Valery <valery22051998@gmail.com>
2021-03-31 15:13:10 +0300

    fix Content.js

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -4,44 +4,43 @@ import { CardContextConsumer } from '../../context/card-context';
 import CardList from './CardList';
 import './Content.css';
 
-const Content = () => {
-  return (
-    <CardContextConsumer>
-      {({
-        cards,
-        isReadOnly,
-        selectCardHandler,
-        addCardHandler,
-        deleteCardHandler,
-        switchReadOnly,
-      }) => (
-        <>
-          <StyledDiv>
-            <StyledCheckbox
-              id="read-only"
-              type="checkbox"
-              checked={isReadOnly}
-              onChange={switchReadOnly}
-            />
+const Content = () => (
+  <CardContextConsumer>
+    {({
+      cards,
+      isReadOnly,
+      selectCardHandler,
+      addCardHandler,
+      deleteCardHandler,
+      switchReadOnly,
+    }) => (
+      <>
+        <StyledDiv>
+          <StyledCheckbox
+            id="read-only"
+            type="checkbox"
+            checked={isReadOnly}
+            onChange={switchReadOnly}
+          />
 
-            <label htmlFor="read-only">Read-Only</label>
+          <label htmlFor="read-only">Read-Only</label>
 
-            <StyledButton onClick={deleteCardHandler}>
-              Delete selected cards
-            </StyledButton>
-            <StyledButton onClick={addCardHandler}>Create new card</StyledButton>
-          </StyledDiv>
+          <StyledButton onClick={deleteCardHandler}>
+            Delete selected cards
+          </StyledButton>
+          <StyledButton onClick={addCardHandler}>Create new card</StyledButton>
+        </StyledDiv>
 
-          <div className="card-list">
-            <CardList
-              isReadOnly={isReadOnly}
-              cards={cards}
-              checkedForDelete={selectCardHandler}
-            />
-          </div>
-        </>
-      )}
-    </CardContextConsumer>
-  );
-};
+        <div className="card-list">
+          <CardList
+            isReadOnly={isReadOnly}
+            cards={cards}
+            checkedForDelete={selectCardHandler}
+          />
+        </div>
+      </>
+    )}
+  </CardContextConsumer>
+);
+
 export default Content;
