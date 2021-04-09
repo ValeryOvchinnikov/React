7da56c008db2a65bd188bfb9da5a6c39b2277cd2
Valery <valery22051998@gmail.com>
2021-04-09 14:59:12 +0300

    fix Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,18 +4,16 @@ import { useSelector } from 'react-redux';
 
 import Navigation from '../Navigation';
 
-const Header = () => {
-  return (
-    <div className="header">
-      <Navigation className="nav" />
+const Header = () => (
+  <div className="header">
+    <Navigation className="nav" />
 
-      <h1 className="title">Header</h1>
+    <h1 className="title">Header</h1>
 
-      <div className="counter">
-        Cards <span>{useSelector(state => state.cards.length)}</span>
-      </div>
+    <div className="counter">
+      Cards <span>{useSelector(state => state.cards.length)}</span>
     </div>
-  );
-};
+  </div>
+);
 
 export default React.memo(Header);
