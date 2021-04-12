814b77e301fa31c86757cb67bbc8f2659a874e15
Valery <valery22051998@gmail.com>
2021-04-12 17:54:13 +0300

    add custom logger

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import cardReducer from './reducers/cardReducer';
 
+const logger = store => next => action => {
+  let result;
+  console.groupCollapsed('dispatching', action.type);
+  console.log('previous state', store.getState());
+  console.log('action', action);
+  // eslint-disable-next-line prefer-const
+  result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const middleware = applyMiddleware(thunk, logger);
 const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(cardReducer, composeEnchancers(applyMiddleware(thunk)));
+const store = createStore(cardReducer, composeEnchancers(middleware));
 
 export default store;
